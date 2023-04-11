8e823886d429f3836f42f405c2c7bcab8f80d084
Extbech <betternerfakali@gmail.com>
2023-04-11 15:10:57 +0200

    yessir

---
+++ model3D.py
@@ -0,0 +1,24 @@
+import tensorflow as tf
+from tensorflow import keras
+from keras import layers
+import numpy as np
+
+def build_3D_model(input_data):
+    model = keras.models.Sequential()
+    model.add(layers.Conv3D(filters=32, kernel_size=3, padding="same", strides=1, activation="relu", input_shape=input_data.shape[1:]))
+    model.add(layers.MaxPool3D(pool_size=2, padding="same"))
+    model.add(layers.Conv3D(filters=3, kernel_size=3, padding="same", activation="relu"))
+    model.add(layers.MaxPool3D(pool_size=2, padding="same"))
+    model.add(layers.Conv3D(filters=3, kernel_size=3, padding="same", activation="relu"))
+    model.add(layers.MaxPool3D(pool_size=2, padding="same"))
+    model.add(layers.Flatten())
+    model.add(layers.Dense(128, activation="relu"))
+    model.add(layers.Dense(2))
+    return model
+
+def compile_3D_model(model, lr):
+    loss = keras.losses.BinaryCrossentropy(from_logits=True)
+    optimizer = keras.optimizers.Adam(learning_rate=lr)
+    metrics=["accuracy"]
+    model.compile(optimizer=optimizer, loss=loss, metrics=metrics)
+    return model
