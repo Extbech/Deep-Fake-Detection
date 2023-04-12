936743bec793e4c5b22224b9392803bca92f1f05
oledybedokken <69799492+oledybedokken@users.noreply.github.com>
2023-04-12 15:35:24 +0200

    BIG BOY PUSH

--- model3D.py
+++ model3D.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def build_3D_model(input_data):
     model = keras.models.Sequential()
-    model.add(layers.Conv3D(filters=32, kernel_size=3, padding="same", strides=1, activation="relu", input_shape=input_data.shape[1:]))
+    model.add(layers.Conv3D(filters=6, kernel_size=3, padding="same", strides=1, activation="relu", input_shape=input_data.shape[1:]))
     model.add(layers.MaxPool3D(pool_size=2, padding="same"))
     model.add(layers.Conv3D(filters=3, kernel_size=3, padding="same", activation="relu"))
     model.add(layers.MaxPool3D(pool_size=2, padding="same"))
